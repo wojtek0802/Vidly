3b2f18e6bf3c90394f58405ea4014dc07de116c9
Wojciech Pracz <wojtek0802@gmail.com>
2019-05-12 15:26:36 +0200

    initial commit

---
+++ Vidly/Controllers/CustomersController.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Vidly.Models;
+
+namespace Vidly.Controllers
+{
+    public class CustomersController : Controller
+    {
+        // GET: Customers
+        
+
+        public ActionResult Index()
+        {
+            var customers = GetCustomers();
+            return View(customers);
+        }
+
+        public ActionResult Details(int id)
+        {
+            var customers = GetCustomers();
+            var customer = customers.SingleOrDefault(c => c.Id == id);
+            if(customer!=null)
+                return View(customer);
+            else
+                return HttpNotFound();
+        }
+
+        private IEnumerable<Customer> GetCustomers()
+        {
+            List<Customer> customers = new List<Customer>
+            {
+                new Customer {Name = "John Smith", Id=1},
+                new Customer {Name ="Mary Williams", Id=2}
+            };
+
+            return customers;
+
+        }
+    }
+}
