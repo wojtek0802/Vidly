1bb5c12862580c991c80eb4eb0fd10d75ca50471
Wojciech Pracz <wojtek0802@gmail.com>
2019-05-12 17:21:02 +0200

    created database and added first migrations

--- Vidly/Controllers/CustomersController.cs
+++ Vidly/Controllers/CustomersController.cs
@@ -1,43 +1,44 @@
-﻿using System;
+﻿ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
 using Vidly.Models;
+using System.Data.Entity;
 
 namespace Vidly.Controllers
 {
     public class CustomersController : Controller
     {
         // GET: Customers
-        
+
+        private ApplicationDbContext _context;
+
+        public CustomersController()
+        {
+            _context = new ApplicationDbContext();
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            _context.Dispose();
+        }
 
         public ActionResult Index()
         {
-            var customers = GetCustomers();
+            var customers = _context.Customers.Include(c=>c.MembershipType).ToList();
             return View(customers);
         }
 
         public ActionResult Details(int id)
         {
-            var customers = GetCustomers();
-            var customer = customers.SingleOrDefault(c => c.Id == id);
+            var customer = _context.Customers.SingleOrDefault(c => c.Id == id);
+            
             if(customer!=null)
                 return View(customer);
             else
                 return HttpNotFound();
         }
 
-        private IEnumerable<Customer> GetCustomers()
-        {
-            List<Customer> customers = new List<Customer>
-            {
-                new Customer {Name = "John Smith", Id=1},
-                new Customer {Name ="Mary Williams", Id=2}
-            };
-
-            return customers;
-
-        }
     }
 }
