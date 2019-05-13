0809e1cd50cd6bbed7afd76119b2ac52ad116a8d
Wojciech Pracz <wojtek0802@gmail.com>
2019-05-13 16:00:10 +0200

    changes added to models and the database

--- Vidly/Controllers/MoviesController.cs
+++ Vidly/Controllers/MoviesController.cs
@@ -5,28 +5,43 @@ using System.Web;
 using System.Web.Mvc;
 using Vidly.Models;
 using Vidly.ViewModels;
+using System.Data.Entity;
 
 namespace Vidly.Controllers
 {
     public class MoviesController : Controller
     {
+        private ApplicationDbContext _context;
+
+        public MoviesController()
+        {
+            _context = new ApplicationDbContext();
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            _context.Dispose();
+        }
+
         //GET: Movies
         public ActionResult Index()
         {
-            var movies = GetMovies();
+            var movies = _context.Movies.Include(m => m.Genre).ToList();
+
             return View(movies);
         }
 
-        private IEnumerable<Movie> GetMovies()
+        public ActionResult Details(int id)
         {
-            List<Movie> movies = new List<Movie>
-            {
-                new Movie() {Name="Shrek!"},
-                new Movie() {Name="Wall-e"}
-            };
+            var movie = _context.Movies.Include(m => m.Genre).SingleOrDefault(m => m.Id == id);
+            if (movie != null)
+                return View(movie);
+            else
+                return HttpNotFound();
 
-            return movies;
         }
+
+        
 
 
         //[Route("movies/released/{year}/{month:regex(\\d{4}):range(1,12)}")]
--- Vidly/Models/Movie.cs
+++ Vidly/Models/Movie.cs
@@ -9,6 +9,11 @@ namespace Vidly.Models
     {
         public int Id { get; set; }
         public string Name { get; set; }
+        public Genre Genre { get; set; }
+        public byte GenreId { get; set; }
+        public DateTime ReleaseDate { get; set; }
+        public DateTime DateAdded { get; set; }
+        public int NumberInStock { get; set; }
     }
 
     //movies/random
